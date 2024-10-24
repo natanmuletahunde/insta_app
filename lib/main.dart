2097fbff9a7a5ca050504f67cdc70c6162d372c8
pivnati <natanmuleta911@gmail.com>
2024-10-23 18:24:29 -0700

    modify the main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -15,10 +15,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: ' Instagram',
-      theme: ThemeData(   
- )
-
-    );
+      theme: ThemeData.dark(),
+      home: Scaffold(body: Text('Lets build Instagram'),)
+ )  ;
   }
 }
