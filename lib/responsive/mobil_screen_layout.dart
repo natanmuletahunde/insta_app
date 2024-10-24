4d75d7cd4f15f1241da3ba3173d06ea7a34f1f46
pivnati <natanmuleta911@gmail.com>
2024-10-23 18:42:39 -0700

    create the mobile and web in the responsive folder

---
+++ lib/responsive/mobil_screen_layout.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+class MobilScreenLayout extends StatelessWidget {
+  const MobilScreenLayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Text('this is mobile'),
+    );
+  }
+}
---
+++ lib/responsive/web_screen_layout.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+class WebScreenLayout extends StatelessWidget {
+  const WebScreenLayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Text('this is web'),
+    );
+  }
+}
