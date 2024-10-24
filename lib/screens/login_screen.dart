96b364b7855ce81c68c794801706853a4934bc45
pivnati <natanmuleta911@gmail.com>
2024-10-24 09:08:00 -0700

    make the images below in the page

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -15,15 +15,20 @@ class _LoginScreenState extends State<LoginScreen> {
         child: Container(
           padding: const EdgeInsets.symmetric(horizontal: 32),
           width: double.infinity,
-          child: const Column(
+          child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-               SizedBox(height: 40), // Add spacing to move the image down
-              CircleAvatar(
+              Flexible(
+                child: Container(),
+                flex: 2,
+              ),
+              const SizedBox(height: 40), // Add spacing to move the image down
+              const CircleAvatar(
                 radius: 50, // Adjust the radius to control the size
                 backgroundImage: AssetImage('assets/images/ig.png'), // Path to the image file
               ),
-               SizedBox(height: 20), // Additional spacing if needed
+              const SizedBox(height: 20), // Additional spacing if needed
+              // You can add more widgets here like TextFields, buttons, etc.
             ],
           ),
         ),
