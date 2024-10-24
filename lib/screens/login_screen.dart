fb86274c4156a435d752a3eaa29cf372b0f7ab08
pivnati <natanmuleta911@gmail.com>
2024-10-24 09:28:19 -0700

    create the input email in the login_screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:instagram/widgets/text_field_input.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -8,7 +9,14 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
   @override
+  void dispose(){
+    super.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+  }
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
@@ -18,17 +26,20 @@ class _LoginScreenState extends State<LoginScreen> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Flexible(
-                child: Container(),
-                flex: 2,
-              ),
-              const SizedBox(height: 40), // Add spacing to move the image down
+               const SizedBox(height: 40), // Add spacing to move the image down
               const CircleAvatar(
                 radius: 50, // Adjust the radius to control the size
                 backgroundImage: AssetImage('assets/images/ig.png'), // Path to the image file
               ),
-              const SizedBox(height: 20), // Additional spacing if needed
-              // You can add more widgets here like TextFields, buttons, etc.
+               const SizedBox(height: 20), // Additional spacing if needed
+               
+              TextFieldInput(
+  hintText: 'Enter your email',
+  textInputType: TextInputType.emailAddress,
+  textEditingController: _emailController,
+  isPass: false, // Since this is an email field, isPass should be false
+)
+
             ],
           ),
         ),
