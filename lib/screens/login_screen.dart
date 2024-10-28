daace296c1d3e723440e986cff6acf5b046539ca
pivnati <natanmuleta911@gmail.com>
2024-10-27 20:40:30 -0700

    solve the overflowed pixel problem

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,5 +1,3 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 import 'package:instagram/resources/auth_methods.dart';
 import 'package:instagram/utils/utils.dart';
@@ -15,6 +13,7 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+
   @override
   void dispose() {
     super.dispose();
@@ -22,95 +21,86 @@ class _LoginScreenState extends State<LoginScreen> {
     _passwordController.dispose();
   }
 
-void loginUser() async {
-  String res = await AuthMethods().loginUser(
-      email: _emailController.text, password: _passwordController.text);
-  if (res == 'success') {
-    //
-  } else {
-    showSnackBar(res, context);
+  void loginUser() async {
+    String res = await AuthMethods().loginUser(
+        email: _emailController.text, password: _passwordController.text);
+    if (res == 'success') {
+      //
+    } else {
+      showSnackBar(res, context);
+    }
   }
-}
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 32),
-          width: double.infinity,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const SizedBox(height: 40), // Add spacing to move the image down
-              const CircleAvatar(
-                radius: 50, // Adjust the radius to control the size
-                backgroundImage: AssetImage(
-                    'assets/images/ig.png'), // Path to the image file
-              ),
-              const SizedBox(height: 20), // Additional spacing if needed
-
-              TextFieldInput(
-                hintText: 'Enter your email',
-                textInputType: TextInputType.emailAddress,
-                textEditingController: _emailController,
-                isPass:
-                    false, // Since this is an email field, isPass should be false
-              ),
-              const SizedBox(height: 20), // Additional spacing if needed
-              TextFieldInput(
-                hintText: 'Enter your password',
-                textInputType: TextInputType.text,
-                textEditingController: _passwordController,
-                isPass:
-                    true, // Since this is an email field, isPass should be false
-              ),
-              const SizedBox(height: 20),
-              InkWell(
-                onTap: loginUser,
-                child: Container(
-                  child: const Text('Log in'),
-                  width: double.infinity,
-                  alignment: Alignment.center,
-                  padding: const EdgeInsets.symmetric(vertical: 12),
-                  decoration: const ShapeDecoration(
-                    color: Colors.blue, // Add background color here
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(4)),
+        child: SingleChildScrollView( // Wrap Column with SingleChildScrollView
+          child: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 32),
+            width: double.infinity,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(height: 40),
+                const CircleAvatar(
+                  radius: 50,
+                  backgroundImage: AssetImage('assets/images/ig.png'),
+                ),
+                const SizedBox(height: 20),
+                TextFieldInput(
+                  hintText: 'Enter your email',
+                  textInputType: TextInputType.emailAddress,
+                  textEditingController: _emailController,
+                  isPass: false,
+                ),
+                const SizedBox(height: 20),
+                TextFieldInput(
+                  hintText: 'Enter your password',
+                  textInputType: TextInputType.text,
+                  textEditingController: _passwordController,
+                  isPass: true,
+                ),
+                const SizedBox(height: 20),
+                InkWell(
+                  onTap: loginUser,
+                  child: Container(
+                    child: const Text('Log in'),
+                    width: double.infinity,
+                    alignment: Alignment.center,
+                    padding: const EdgeInsets.symmetric(vertical: 12),
+                    decoration: const ShapeDecoration(
+                      color: Colors.blue,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(4)),
+                      ),
                     ),
                   ),
                 ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    padding: const EdgeInsets.symmetric(vertical: 8),
-                    child: const Text('Do not have an account?  '),
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      // Define the action for the tap here
-                    },
-                    child: Container(
-                      // First padding is correct
-                      child: const Text(
-                        'Sign Up',
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                      padding: const EdgeInsets.symmetric(
-                        // Second padding should use a comma, not a semicolon
-                        vertical: 8,
-                      ),
+                const SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.symmetric(vertical: 8),
+                      child: const Text('Do not have an account?  '),
                     ),
-                  )
-                ],
-              )
-            ],
+                    GestureDetector(
+                      onTap: () {
+                        // Define the action for the tap here
+                      },
+                      child: Container(
+                        child: const Text(
+                          'Sign Up',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        padding: const EdgeInsets.symmetric(vertical: 8),
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
