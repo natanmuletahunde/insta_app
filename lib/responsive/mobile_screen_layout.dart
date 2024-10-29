b2dbbd50ea75f8b2396c6efd12bebf26af85cfd1
pivnati <natanmuleta911@gmail.com>
2024-10-28 20:13:45 -0700

    modify the user_provider and mobile_screesn

--- lib/responsive/mobile_screen_layout.dart
+++ lib/responsive/mobile_screen_layout.dart
@@ -1,8 +1,6 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:instagram/providers/user_provider.dart';
 import 'package:instagram/models/user.dart' as model;
+import 'package:instagram/providers/user_provider.dart';
 import 'package:provider/provider.dart';
 
 class MobileScreenLayout extends StatefulWidget {
@@ -14,8 +12,9 @@ class MobileScreenLayout extends StatefulWidget {
 
 class _MobileScreenLayoutState extends State<MobileScreenLayout> {
   @override
+
   Widget build(BuildContext context) {
-    model.User user = Provider.of<UserProvider>(context).getUser;
+      model.User user = Provider.of<UserProvider>(context).getUser;
     return  Scaffold(
       body: Center(
         child: Text(user.username),
