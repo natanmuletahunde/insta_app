bfdca1034200be23d3d300b103c8b3936b0ffa89
pivnati <natanmuleta911@gmail.com>
2024-10-28 21:42:19 -0700

    modify the dimesion to global varibales

---
+++ lib/screens/add_post_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AddPostScreen extends StatefulWidget {
+  const AddPostScreen({super.key});
+
+  @override
+  State<AddPostScreen> createState() => _AddPostScreenState();
+}
+
+class _AddPostScreenState extends State<AddPostScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: IconButton(onPressed: (){}, icon: const Icon(Icons.upload)),
+    );
+  }
+}
