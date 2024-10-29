c0a2ef4373a58d734848cd7e28940c742e9857f7
pivnati <natanmuleta911@gmail.com>
2024-10-29 10:14:32 -0700

    create the the diver() in the add_post page

--- lib/screens/add_post_screen.dart
+++ lib/screens/add_post_screen.dart
@@ -44,6 +44,45 @@ class _AddPostScreenState extends State<AddPostScreen> {
           ),
         ],
       ),
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CircleAvatar(
+                backgroundImage: NetworkImage(
+                    'https://plus.unsplash.com/premium_photo-1670876808488-db44fb4a12d3?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'),
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.45,
+                child: TextField(
+                  decoration: const InputDecoration(
+                      hintText: ' Write a caption....',
+                      border: InputBorder.none),
+                  maxLines: 8,
+                ),
+              ),
+              SizedBox(
+                height: 45,
+                width: 45,
+                child: AspectRatio(
+                  aspectRatio: 487 / 451,
+                  child: Container(
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: NetworkImage(
+                                'https://plus.unsplash.com/premium_photo-1670876808488-db44fb4a12d3?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'),
+                            fit: BoxFit.fill,
+                            alignment: FractionalOffset.topCenter)),
+                  ),
+                ),
+              ),
+                const Divider()
+            ],
+          )
+        ],
+      ),
     );
   }
 }
