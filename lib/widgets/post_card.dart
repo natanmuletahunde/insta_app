6aa4d4f7ea8f0660a7fff8cdaafaa7a044e8c620
pivnati <natanmuleta911@gmail.com>
2024-10-30 11:13:38 -0700

    create the iconbutton in the post_card.dart

--- lib/widgets/post_card.dart
+++ lib/widgets/post_card.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:instagram/utils/colors.dart';
 
@@ -15,6 +13,7 @@ class PostCard extends StatelessWidget {
       ),
       child: Column(
         children: [
+          // Header section
           Container(
             padding: const EdgeInsets.symmetric(
               vertical: 4,
@@ -40,32 +39,68 @@ class PostCard extends StatelessWidget {
                     ],
                   ),
                 )),
-               IconButton(
-  onPressed: () {
-    showDialog(
-      context: context,
-      builder: (context) => Dialog(
-        child: ListView(
-          padding: const EdgeInsets.symmetric(vertical: 16),
-          shrinkWrap: true,
-          children: [
-            'Delete',
-          ].map(
-            (e) => InkWell(
-              onTap: () {},
-              child: Container(
-                padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
-                child: Text(e),
+                IconButton(
+                  onPressed: () {
+                    showDialog(
+                      context: context,
+                      builder: (context) => Dialog(
+                        child: ListView(
+                          padding: const EdgeInsets.symmetric(vertical: 16),
+                          shrinkWrap: true,
+                          children: [
+                            'Delete',
+                          ]
+                              .map(
+                                (e) => InkWell(
+                                  onTap: () {},
+                                  child: Container(
+                                    padding: const EdgeInsets.symmetric(
+                                        vertical: 12, horizontal: 16),
+                                    child: Text(e),
+                                  ),
+                                ),
+                              )
+                              .toList(), // Added parentheses here
+                        ),
+                      ),
+                    );
+                  },
+                  icon: const Icon(Icons.more_vert),
+                ),
+              ],
+            ),
+          ),
+          // Image section
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.35,
+            width: double.infinity,
+            child: Image.network(
+              'https://images.unsplash.com/photo-1517594321692-b9ac1abbdd12?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+              fit: BoxFit.cover,
+            ),
+          ),
+
+          // Like Comment Section
+          Row(
+            children: [
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.favorite,
+                  color: Colors.red,
+                ),
               ),
-            ),
-          ).toList(), // Added parentheses here
-        ),
-      ),
-    );
-  },
-  icon: const Icon(Icons.more_vert),
-),],
-            ),
+              IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.comment_outlined,
+                  )),
+              IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.send,
+                  )),
+            ],
           )
         ],
       ),
