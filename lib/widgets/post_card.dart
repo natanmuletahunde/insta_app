d75a115dc9f04bf322fef062f16ae2b4f88d3c5c
pivnati <natanmuleta911@gmail.com>
2024-10-30 10:47:57 -0700

    modify the post_card.dart file

--- lib/widgets/post_card.dart
+++ lib/widgets/post_card.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:instagram/utils/colors.dart';
 
@@ -7,7 +9,6 @@ class PostCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-
       color: mobileBackgroundColor,
       padding: const EdgeInsets.symmetric(
         vertical: 10,
@@ -18,26 +19,53 @@ class PostCard extends StatelessWidget {
             padding: const EdgeInsets.symmetric(
               vertical: 4,
               horizontal: 16,
-            ).copyWith( right: 0),
+            ).copyWith(right: 0),
             child: Row(
               children: [
                 CircleAvatar(
                   radius: 16,
                   backgroundImage: NetworkImage(''),
                 ),
-                Expanded(child: 
-                Padding(padding: const EdgeInsets.only(left: 8),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('username',style: TextStyle(fontWeight:FontWeight.bold ),)
-                    ,
-                  ],
-                ),))
-              ],
+                Expanded(
+                    child: Padding(
+                  padding: const EdgeInsets.only(left: 8),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'username',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                )),
+               IconButton(
+  onPressed: () {
+    showDialog(
+      context: context,
+      builder: (context) => Dialog(
+        child: ListView(
+          padding: const EdgeInsets.symmetric(vertical: 16),
+          shrinkWrap: true,
+          children: [
+            'Delete',
+          ].map(
+            (e) => InkWell(
+              onTap: () {},
+              child: Container(
+                padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+                child: Text(e),
+              ),
             ),
-            
+          ).toList(), // Added parentheses here
+        ),
+      ),
+    );
+  },
+  icon: const Icon(Icons.more_vert),
+),],
+            ),
           )
         ],
       ),
