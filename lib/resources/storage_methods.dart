4689ae175ab0a4a62a18ce82c8cb0c0e814dfcf5
pivnati <natanmuleta911@gmail.com>
2024-10-29 20:33:51 -0700

    modify the ref in the storage file

--- lib/resources/storage_methods.dart
+++ lib/resources/storage_methods.dart
@@ -15,7 +15,7 @@ class StorageMethods {
        Reference ref = _storage.ref().child(childName).child(_auth.currentUser!.uid);
          if(isPost){
           String id = const Uuid().v1();
-          ref.child(id);
+          ref = ref.child(id);
          }
 
       UploadTask uploadTask = ref.putData(file);
