a7b7929fe7cb0e057e48c647fcd32ae3679cc920
pivnati <natanmuleta911@gmail.com>
2024-10-30 05:58:35 -0700

    add the feed_icon in assets

--- lib/screens/feed_screen.dart
+++ lib/screens/feed_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:instagram/utils/colors.dart';
 
 class FeedScreen extends StatelessWidget {
   const FeedScreen({super.key});
@@ -8,6 +10,16 @@ class FeedScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        appBar: AppBar(
+          backgroundColor: mobileBackgroundColor,
+          centerTitle: false,
+          title: SvgPicture.asset('',
+          color: primaryColor,
+          height: 32,),
+          actions: [
+            IconButton(onPressed: (){}, icon: Icon(Icons.message_outlined),),
+          ],
+        ),
       
     );
   }
