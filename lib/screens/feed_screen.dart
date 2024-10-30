8d90946ce39352fba18c6f19483a3b5645620576
pivnati <natanmuleta911@gmail.com>
2024-10-30 15:17:33 -0700

    create the streambuilder to connect with the firebasestore

--- lib/screens/feed_screen.dart
+++ lib/screens/feed_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram/utils/colors.dart';
 import 'package:instagram/widgets/post_card.dart';
@@ -8,27 +9,38 @@ class FeedScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-  appBar: AppBar(
-    backgroundColor: mobileBackgroundColor,
-    centerTitle: false,
-    title: const Text(
-      'Instagram',
-      style: TextStyle(
-        color: primaryColor, 
-        fontSize: 24, 
-        fontWeight: FontWeight.bold, 
+      appBar: AppBar(
+        backgroundColor: mobileBackgroundColor,
+        centerTitle: false,
+        title: const Text(
+          'Instagram',
+          style: TextStyle(
+            color: primaryColor,
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.message_outlined),
+          ),
+        ],
       ),
-    ),
-    actions: [
-      IconButton(
-        onPressed: () {},
-        icon: Icon(Icons.message_outlined),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection('posts').snapshots(),
+        builder: (context, AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {  // the containn the 
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return ListView.builder(
+            itemCount: snapshot.data!.docs.length,
+            itemBuilder: (context, index) => PostCard(),
+          );
+        },
       ),
-    ],
-  ),
-
-  body:const PostCard() ,
-);
-
+    );
   }
 }
