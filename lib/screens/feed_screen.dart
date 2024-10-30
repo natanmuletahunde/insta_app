435503aa6fa69bd61eca4510db33594b40eaca28
pivnati <natanmuleta911@gmail.com>
2024-10-30 05:47:39 -0700

    create the FeedScreen in the global vriable

---
+++ lib/screens/feed_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+class FeedScreen extends StatelessWidget {
+  const FeedScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
