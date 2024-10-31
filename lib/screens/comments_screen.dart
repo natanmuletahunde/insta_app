90df3de7fe06560f395f64b786897662a163a588
pivnati <natanmuleta911@gmail.com>
2024-10-31 14:39:08 -0700

    create the textfield  inside the comment_card.dart

--- lib/screens/comments_screen.dart
+++ lib/screens/comments_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:instagram/utils/colors.dart';
+import 'package:instagram/widgets/comment_card.dart';
 
 class CommentsScreen extends StatefulWidget {
   const CommentsScreen({super.key});
@@ -31,6 +32,26 @@ class _CommentsScreenState extends State<CommentsScreen> {
               backgroundImage: NetworkImage(
                   'https://images.unsplash.com/photo-1730051316601-4c71c894e496?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGNvbWV0fGVufDB8fDB8fHww'),
               radius: 18,
+            ),
+            Padding(padding: const EdgeInsets.only(left: 16),
+               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  RichText(text: TextSpan(
+                    children: [
+                      TextSpan(
+                        text: 'username',
+                        style: const TextStyle(fontWeight: FontWeight.bold)
+                      ),
+                       TextSpan(
+                        text: 'username',
+                        style: const TextStyle(fontWeight: FontWeight.bold)
+                      )
+                    ]
+                  ))
+                ],
+               ),
             ),
             Expanded(
               child: Padding(
--- lib/widgets/comment_card.dart
+++ lib/widgets/comment_card.dart
@@ -10,6 +10,19 @@ class CommentCard extends StatefulWidget {
 class _CommentCardState extends State<CommentCard> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      padding: const EdgeInsets.symmetric(
+        vertical: 18,
+        horizontal: 16
+      ),
+      child:Row(
+        children: [
+          CircleAvatar(
+            backgroundImage: NetworkImage('https://images.unsplash.com/photo-1730051316601-4c71c894e496?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGNvbWV0fGVufDB8fDB8fHww'),
+              radius: 18,
+          )
+        ],
+      ),
+    );
   }
 }
