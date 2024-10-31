917fdf1fac466c228e5d208fb2fa4909df489a51
pivnati <natanmuleta911@gmail.com>
2024-10-30 19:56:01 -0700

    add the scale and the duration for the likeanimation

--- lib/widgets/like_animation.dart
+++ lib/widgets/like_animation.dart
@@ -1,20 +1,41 @@
 import 'package:flutter/material.dart';
 
 class LikeAnimation extends StatefulWidget {
-  const LikeAnimation({super.key});
+  final Widget child;
+  final bool isAnimating;
+  final Duration duration;
+  final VoidCallback? onEnd;
+  final bool smallLike;
+
+  const LikeAnimation({
+    Key? key,
+    required this.child,
+    required this.isAnimating,
+    this.duration = const Duration(milliseconds: 150),
+    this.onEnd,
+    this.smallLike = false,
+  }) : super(key: key);
 
   @override
   State<LikeAnimation> createState() => _LikeAnimationState();
 }
 
-class _LikeAnimationState extends State<LikeAnimation> {
+class _LikeAnimationState extends State<LikeAnimation> with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  late Animation<double> scale;
 
-   
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      vsync: this,
+      duration: Duration( milliseconds: widget.duration.inMicroseconds ~/2)
+    );
+    scale = Tween<double>(begin:1, end: 1.2).animate(controller);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-
-
-    );
+    return Container(); 
   }
 }
