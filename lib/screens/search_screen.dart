3608642edef84006f102fcb33656d8a18c0214dc
pivnati <natanmuleta911@gmail.com>
2024-11-01 05:33:29 -0700

    create and modify the search_file

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram/utils/colors.dart';
 
@@ -9,17 +10,55 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _SearchScreenState extends State<SearchScreen> {
+  final TextEditingController searchController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    searchController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-     appBar: AppBar(
-      backgroundColor: mobileBackgroundColor,
-      title: TextFormField(
-        decoration: const InputDecoration(
-          labelText: 'Search for a user'
+      appBar: AppBar(
+        backgroundColor: mobileBackgroundColor,
+        title: TextFormField(
+          controller: searchController,
+          decoration: const InputDecoration(
+            labelText: 'Search for a user',
+          ),
+          onFieldSubmitted: (String _) {
+            print(_);
+          },
         ),
       ),
-     ),
+      body: FutureBuilder(
+        future: FirebaseFirestore.instance
+            .collection('users')
+            .where('username', isGreaterThanOrEqualTo: searchController.text)
+            .get(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return ListView.builder(
+            itemCount: (snapshot.data! as dynamic).docs.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                leading: CircleAvatar(
+                  backgroundImage: NetworkImage(
+                    (snapshot.data! as dynamic).docs[index]['photoUrl'],
+                  ),
+                ),
+                title: Text((snapshot.data! as dynamic).docs[index]['username']),
+              );
+            },
+          );
+        },
+      ),
     );
   }
 }
