88fcf85b41f1e1c625f5eeba505fc470de280e47
pivnati <natanmuleta911@gmail.com>
2024-11-02 10:54:32 -0700

    get the icons in the webscreen

--- lib/responsive/web_screen_layout.dart
+++ lib/responsive/web_screen_layout.dart
@@ -1,13 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:instagram/utils/colors.dart';
 class WebScreenLayout extends StatelessWidget {
   const WebScreenLayout({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-body: Center(
-  child: Text(' This is  web'),
-),
-    );
+    return  Scaffold
+        (
+         appBar: AppBar(
+        backgroundColor: mobileBackgroundColor,
+        centerTitle: false,
+        title: const Text(
+          'Instagram',
+          style: TextStyle(
+            color: primaryColor,
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.home),
+          ),
+           IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.search),
+          ),
+           IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.add_a_photo),
+          ),
+           IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.favorite),
+          ),
+           IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.person),
+          ),
+        ],
+      ),
+          body: Text('web here'),
+        );
+    
   }
 }
