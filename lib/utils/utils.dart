e4b39dd9624b197bb911f973612a815ee0effce2
pivnati <natanmuleta911@gmail.com>
2024-11-21 21:57:15 -0700

    modify the utils.dart in the image selcted section

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -2,15 +2,15 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
- pickImage(ImageSource source) async { // Mark as async, return Future<File?>
+ pickImage(ImageSource source) async { 
   final ImagePicker _imagePicker = ImagePicker();
 
   XFile? _file = await _imagePicker.pickImage(source: source);
-  if (_file != null) { // Corrected null check syntax
-    return await _file.readAsBytes(); // Convert XFile to File and return
+  if (_file != null) { 
+    return await _file.readAsBytes(); 
   } else {
     print('No image selected');
-    return null; // Return null if no image is selected
+    return null;
   }
 }
 
